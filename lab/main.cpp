53b361335ef089979193ef922efede9b77700abf
Ivan Sladkov <ivan.sladkov@yandex.ru>
2022-05-18 23:34:25 +0300

    Sequential part works.

--- lab/main.cpp
+++ lab/main.cpp
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <vector>
 #include "matplotlibcpp.h"
@@ -33,6 +34,7 @@ void abortAll(int rc, string str){
 }
 
 vector<vector<double>> solve(int rank, int numprocs){
+	cout<<rank<<" "<<numprocs<<endl;
 	if ((M-1)%numprocs==0){
 		brickSize=(M-1)/numprocs;
 		lowSize = brickSize;
@@ -54,19 +56,23 @@ vector<vector<double>> solve(int rank, int numprocs){
 	for (int i=0; i<(rank==numprocs-1?lowSize:brickSize); i++)
 		field[0][i]=t0(i*h);
 	if (rank==0)
-		for (int i=0; i<N; i++)
+		for (int i=0; i<N; i++){
 			field[i][0]=x0(i*tau);
+		}
 	for (int n=0; n<N-1; n++){
 		double left, right;
 		if (rank!=numprocs-1){
 			if (rank!=0)
 				MPI_Recv(&left, 1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &status);
-			else
+			else{
 				left=x0(n*tau);
+				field[n][0]=left;
+			}
 			field[n+1][0]=f(n*tau, (rank*brickSize)*h)+field[n][0]-
 					tau/2/h*(field[n][1]-left)+
 					tau*tau/2/h/h*(field[n][1]-2*field[n][0]+
 					left);
+			cout<<field[n+1][0]<<endl;
 			if (rank!=0)
 				MPI_Isend(&field[n+1][0], 1, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, &request);
 			for (int m=1; m<brickSize-1; m++){
@@ -84,17 +90,26 @@ vector<vector<double>> solve(int rank, int numprocs){
 			MPI_Isend(&field[n+1][m], 1, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, &request);
 		}
 		else {
-			MPI_Recv(&left, 1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &status);
-			field[n+1][0]=f(n*tau, (rank*brickSize)*h)+field[n][0]-
-					tau/2/h*(field[n][1]-left)+
-					tau*tau/2/h/h*(field[n][1]-2*field[n][0]+
-					left);
-			MPI_Isend(&field[n+1][0], 1, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, &request);
+			if (rank!=0){
+				MPI_Recv(&left, 1, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &status);
+				field[n+1][0]=f(n*tau, (rank*brickSize)*h)+field[n][0]-
+						tau/2/h*(field[n][1]-left)+
+						tau*tau/2/h/h*(field[n][1]-2*field[n][0]+
+						left);
+
+			}
+			else{
+				left=x0(n*tau);
+				field[n][0]=left;
+			}
+			if (rank!=0)
+				MPI_Isend(&field[n+1][0], 1, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, &request);
 			for (int m=1; m<lowSize-1; m++){
 				field[n+1][m]=f(n*tau, (rank*brickSize+m)*h)+field[n][m]-
 					tau/2/h*(field[n][m+1]-field[n][m-1])+
 					tau*tau/2/h/h*(field[n][m+1]-2*field[n][m]+
 					field[n][m-1]);
+//				cout<<field[n+1][m]<<endl;
 			}
 			int m=lowSize-1;
 			field[n+1][m]=field[n][m]+f(n*tau, (m)*h)*tau-
@@ -102,6 +117,35 @@ vector<vector<double>> solve(int rank, int numprocs){
 		}
 	}
 	return field;
+}
+
+void save(vector<vector<double>> field, int N, int M){
+	ofstream fout("output.txt");
+	for (int m=0; m<M; m++)
+		for (int n=0; n<N; n++)
+			fout<<field[n][m]<<" ";
+	fout.close();
+}
+
+void draw(int N, int M){
+	ifstream fin("output.txt");
+	std::vector<std::vector<double>> x, y, z;
+	for (int m = 0; m < M;  m++) {
+		std::vector<double> x_row, y_row, z_row;
+		for (int n = 0; n < N; n++) {
+			x_row.push_back(n*tau);
+			y_row.push_back(m*h);
+			double o;
+			fin>>o;
+			z_row.push_back(o);
+		}
+		x.push_back(x_row);
+		y.push_back(y_row);
+		z.push_back(z_row);
+	}
+
+    plt::plot_surface(x, y, z);
+    plt::show();
 }
 
 int main(int argc, char* argv[]){
@@ -117,9 +161,17 @@ int main(int argc, char* argv[]){
 	tau = atof(argv[2]);
 	X = atof(argv[3]);
 	h = atof(argv[4]);
-	solve(0, 1);
-	solve(rank, numprocs);
-
-	MPI_Finalize();
+	N = T/tau;
+	M = X/h;
+	if (rank==0){
+		vector<vector<double>> field = solve(0, 1);
+		save(field, N, M);
+		draw(N, M);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+/*	 solve(rank, numprocs);*/
+/*	if (rank==0){
+	}
+*/	MPI_Finalize();
 	return 0;
 }
